8bae269d6835a180d2dfa4fedfbed33d78da6eee
penn <jenkspt@gmail.com>
2023-01-13 13:35:27 -0800

    add utils for converting hf model parameters

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
-from typing import Any, Optional
+from typing import Any, Optional, Tuple
 from dataclasses import dataclass
 from functools import partial
 import jax
 import jax.numpy as jnp
 import flax.linen as nn
+from flax.core import FrozenDict, freeze, unfreeze
+from flax.traverse_util import flatten_dict, unflatten_dict
 
 
 @dataclass(frozen=True)
@@ -117,3 +119,47 @@ class GPT(nn.Module):
         tokens = jnp.zeros((2, self.config.block_size), dtype=jnp.uint16)
         params = jax.jit(super().init, static_argnums=(2,))(rng, tokens, True)
         return params
+
+
+def convert_hf_params(hf_params: FrozenDict, num_heads, num_embeds) -> FrozenDict:
+    params = unfreeze(hf_params['transformer'])
+    for k, v in params.pop('h', {}).items():
+        params[k] = v
+
+    params = flatten_dict(params, sep='.')
+    for k in params.keys():
+        #if k.endswith('attn.c_attn.bias'):
+        #    params[k] = params[k].reshape(num_heads, -1)
+        if k.endswith('attn.c_attn.kernel'):
+            #params[k] = params[k].reshape(num_embeds, num_heads, -1) 
+            params[k] = params[k].T
+        elif k.endswith('attn.c_proj.kernel'):
+            #params[k] = params[k].reshape(num_heads, -1, num_embeds)
+            params[k] = params[k].T
+        elif k.split('.')[1] == 'mlp' and k.endswith('kernel'):
+            params[k] = params[k].T
+
+    params = unflatten_dict({f'params.{k}': v for k, v in params.items()}, sep='.')
+    return freeze(params)
+
+
+def get_pretrained_params(model_type: str) -> Tuple[GPTConfig, FrozenDict]:
+    """
+    returns config and pretrained parameters from huggingface gpt models 
+    """
+    assert model_type in ('gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl')
+    # only dropout can be overridden see more notes below
+    from transformers import FlaxGPT2LMHeadModel
+    print("loading weights from pretrained gpt: %s" % model_type)
+
+    config = {
+        'gpt2':         GPTConfig(num_layers=12, num_heads=12, num_embeds=768),  # 124M params
+        'gpt2-medium':  GPTConfig(num_layers=24, num_heads=16, num_embeds=1024), # 350M params
+        'gpt2-large':   GPTConfig(num_layers=36, num_heads=20, num_embeds=1280), # 774M params
+        'gpt2-xl':      GPTConfig(num_layers=48, num_heads=25, num_embeds=1600), # 1558M params
+    }[model_type]
+
+    model_hf = FlaxGPT2LMHeadModel.from_pretrained(model_type)
+    hf_params = model_hf.params['transformer']
+    params = convert_hf_params(hf_params, config.num_heads, config.num_embeds)
+    return config, params
